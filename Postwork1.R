01868a82b4f4e826dd17c46596cd6eb527fadee2
adrianchso <71855926+adrianchso@users.noreply.github.com>
2021-02-04 21:49:21 -0600

    Add files via upload

---
+++ Postwork1.R
@@ -0,0 +1,34 @@
+setwd('D:/ACS/BEDU/Modulo 2')
+la.liga<-read.csv('SP1.csv')
+la.liga
+x<-la.liga$FTHG
+x
+y<-la.liga$FTAG
+goles<-data.frame(local = x,visitante = y)
+contl<-table(goles$local)
+totall<-sum(contl)
+z<-rep(totall,7)
+freql<-contl/z
+freql
+contv<- table(goles$visitante)
+c<-c(0)
+names(c)<-c(6)
+c
+contvc<-c(contv,c)
+contvc
+z
+freqv<-contvc/z
+freqv
+tabla<-rbind(freqv,freql)
+row.names(tabla)<-c("Frec. Rel. Goles Local","Frec. Rel. Goles Visitante")
+conj<-matrix(0,7,7,T)
+for (i in 1:7) {
+  for (j in 1:7) {
+    conj[i,j]=freql[i]*freqv[j]
+  }
+}
+conj
+matriz_conj<-matrix(conj,7,7,T)
+matriz_conj
+sum(conj)
+?matrix
